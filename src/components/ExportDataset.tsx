a73667d075cf66cdca40253f0a8acda41475ea19
Tarek Mo <itistarek@gmail.com>
2024-01-10 09:45:20 +0000

    after modification

--- src/components/ExportDataset.tsx
+++ src/components/ExportDataset.tsx
@@ -8,11 +8,24 @@ type ExportDatasetProps = {
 };
 
 const ExportDataset = ({ datasetId, fileType }: ExportDatasetProps) => {
+  console.log("file type", fileType);
+
   const toast = useToast();
   const { mutate, isLoading } = useMutation({
-    mutationFn: () => exportDataset({ datasetId, fileType }),
+    mutationFn: () => exportDataset({ datasetId, fileType: "csv" }),
     onSuccess: (data) => {
       console.log("export success", data);
+      const blob = new Blob([data], { type: "text/csv" });
+      // Create a temporary anchor element
+      const downloadLink = window.document.createElement("a");
+      // Set the URL to the blob
+      downloadLink.href = window.URL.createObjectURL(blob);
+      // Set the filename for the downloaded file
+      downloadLink.download = `exported_dataset.csv`;
+      // Simulate click on the anchor to trigger download
+      downloadLink.click();
+      // Clean up by revoking the object URL
+      window.URL.revokeObjectURL(downloadLink.href);
     },
     onError: (error) => {
       console.log("export error", error);
